8c81dd8bd1cc3a086ceb2a5ee6228d254e92cd05
miikeHcai <2545150201@qq.com>
2021-12-10 20:56:39 +0800

    the second version!

--- bubblesort.py
+++ bubblesort.py
@@ -1,7 +1,14 @@
---bubblesort
-def bubblesort(x-->list):
-  for i in range(len(x)-1,0,-1):
-    for j in range(i-1):
-      if x[j]>x[j+1]:
-        x[j],x[j+1]=x[j+1],x[j]
-  return x
+# bubble sort
+
+
+def bubble_sort(x):
+    for i in range(len(x) - 1, 0, -1):
+        for j in range(i - 1):
+            if x[j] > x[j + 1]:
+                x[j], x[j + 1] = x[j + 1], x[j]
+    return x
+
+
+if __name__ == '__main__':
+    x = [3, 1, 2, 4, 5]
+    print(bubble_sort(x))
